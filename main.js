6cad9fb05418b8e249c33eadc57d38a5d1bd7021
Space-Raja <93535534+Space-Raja@users.noreply.github.com>
2023-02-23 20:15:42 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+function draw(){
+    image(camera, 0, 0, 300, 250);
+
+model.classify(camera, gotResult);
+
+}
+
+function gotResult(error, results){
+    if(error){
+        console.error(error);
+    }
+    else {
+        console.log(results);
+
+document.getElementById("object_name").innerHTML=results[0].label;
+document.getElementById("object_accuracy").innerHTML=results[0].confidence.toFixed(2);
+
+    }
+}
+
+function setup(){
+    canvas = createCanvas(300,250);
+    canvas.position(610,300);
+       camera = createCapture(VIDEO);
+    camera.hide();
+
+model = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/Oowrhusla/model.json", modelLoaded);
+
+}
+
+function modelLoaded(){
+    console.log("Model Loaded!");
+}
+
